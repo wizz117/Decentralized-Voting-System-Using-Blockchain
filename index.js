74c50c1e2558c93fde0c6bc95ba7953e055d814b
Vishnu <95639719+wizz117@users.noreply.github.com>
2025-04-25 16:18:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const app = express();
+
+
+const authorizeUser = (req, res, next) => {
+  const token = req.query.Authorization?.split('Bearer ')[1];
+
+  if (!token) {
+    return res.status(401).send('<h1 align="center"> Login to Continue </h1>');
+  }
+  
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY, { algorithms: ['HS256'] });
+
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid authorization token' });
+  }
+};
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/html/login.html'));
+});
+
+app.get('/js/login.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/js/login.js'))
+});
+
+app.get('/css/login.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/css/login.css'))
+});
+
+app.get('/css/index.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/css/index.css'))
+});
+
+app.get('/css/admin.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/css/admin.css'))
+});
+
+app.get('/assets/eth5.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/assets/eth5.jpg'))
+});
+
+app.get('/js/app.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/js/app.js'))
+});
+
+app.get('/admin.html', authorizeUser, (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/html/admin.html'));
+});
+
+app.get('/index.html', authorizeUser, (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/html/index.html'));
+});
+
+app.get('/dist/login.bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/dist/login.bundle.js'));
+});
+
+app.get('/dist/app.bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/dist/app.bundle.js'));
+});
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/favicon.ico'));
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on http://localhost:8080');
+});
